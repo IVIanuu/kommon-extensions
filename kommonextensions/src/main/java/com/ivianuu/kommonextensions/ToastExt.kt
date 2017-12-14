1887d33f60ab6fa89043fe42345cb505bddb63eb
IVIanuu <IVIanuu@gmail.com>
2017-12-14 01:15:32 +0100

    Dozens of changes

--- kommonextensions/src/main/java/com/ivianuu/kommonextensions/ToastExt.kt
+++ kommonextensions/src/main/java/com/ivianuu/kommonextensions/ToastExt.kt
@@ -32,16 +32,20 @@ internal inline fun Context.make(@StringRes message: Int, duration: Int): Toast 
 
 inline fun Context.toast(message: CharSequence, length: Int = LENGTH_SHORT): Toast = make(message, length).apply { show() }
 inline fun Fragment.toast(message: CharSequence): Toast = activity.toast(message)
+inline fun android.support.v4.app.Fragment.toast(message: CharSequence): Toast = activity.toast(message)
 inline fun Dialog.toast(message: CharSequence): Toast = context.toast(message)
 
 inline fun Context.toast(@StringRes message: Int): Toast = make(message, LENGTH_SHORT).apply { show() }
 inline fun Fragment.toast(@StringRes message: Int): Toast = activity.toast(message)
+inline fun android.support.v4.app.Fragment.toast(@StringRes message: Int): Toast = activity.toast(message)
 inline fun Dialog.toast(@StringRes message: Int): Toast = context.toast(message)
 
 inline fun Context.longToast(message: CharSequence): Toast = make(message, LENGTH_LONG).apply { show() }
 inline fun Fragment.longToast(message: CharSequence): Toast = activity.longToast(message)
+inline fun android.support.v4.app.Fragment.longToast(message: CharSequence): Toast = activity.longToast(message)
 inline fun Dialog.longToast(message: CharSequence): Toast = context.longToast(message)
 
 inline fun Context.longToast(@StringRes message: Int): Toast = make(message, LENGTH_LONG).apply { show() }
 inline fun Fragment.longToast(@StringRes message: Int): Toast = activity.longToast(message)
+inline fun android.support.v4.app.Fragment.longToast(@StringRes message: Int): Toast = activity.longToast(message)
 inline fun Dialog.longToast(@StringRes message: Int): Toast = context.longToast(message)
